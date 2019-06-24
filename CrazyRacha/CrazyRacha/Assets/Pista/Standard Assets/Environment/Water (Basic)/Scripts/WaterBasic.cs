84ed498a527b8253c29bcbaab5bc1ecc528c323d
Mateus <fabriciomateus05@gmail.com>
2019-06-23 21:07:28 -0300

    Modificado Terreno.asset Adicionado assets

--- CrazyRacha/CrazyRacha/Assets/Pista/Standard Assets/Environment/Water (Basic)/Scripts/WaterBasic.cs
+++ CrazyRacha/CrazyRacha/Assets/Pista/Standard Assets/Environment/Water (Basic)/Scripts/WaterBasic.cs
@@ -21,7 +21,7 @@ namespace UnityStandardAssets.Water
 
             Vector4 waveSpeed = mat.GetVector("WaveSpeed");
             float waveScale = mat.GetFloat("_WaveScale");
-            float t = Time.time / 20.0f;
+            float t = Time.time / 40.0f;
 
             Vector4 offset4 = waveSpeed * (t * waveScale);
             Vector4 offsetClamped = new Vector4(Mathf.Repeat(offset4.x, 1.0f), Mathf.Repeat(offset4.y, 1.0f),
